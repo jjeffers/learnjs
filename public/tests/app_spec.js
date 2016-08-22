3bd155a0bb7796b6bad5606f7d09d930a3312169
James Jeffers <jdjeffers@gmail.com>
2016-08-22 10:33:04 -0400

    evaluates tests

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -32,9 +32,23 @@ describe('LearnJS', function() {
 });
 
 describe('problem view', function() {
+
+  var view;
+  
+  beforeEach(function() {
+    view = learnjs.problemView('1');
+  });
   it('has a title that includes the problem number',
   function() {
-    var view = learnjs.problemView('1');
-    expect(view.text()).toEqual('Problem #1 Coming soon!');
+
+    expect(view.text()).toContain(learnjs.problems[0].code);
+  });
+
+  describe('answer section', function() {
+    it('can check a correct answer by hitting a button', function () {
+      view.find('.answer').val('true');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Correct!');
+    });
   });
 });
